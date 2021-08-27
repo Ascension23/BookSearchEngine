c20e2a9aebc825e526534b2282f74fbc9fb3adcd
Adrian Aguilar <Tygor23@gmail.com>
2021-08-27 10:03:50 -0700

    redeploying

--- server/server.js
+++ server/server.js
@@ -20,19 +20,11 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/safe-shore-90904',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  },
-);
+
 // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 app.use(routes);
 
